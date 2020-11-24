9c8acdb34ea07d744420faf75fabc1172ed09f65
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2020-11-24 12:14:28 -0600

    first commit

---
+++ hamburguer/main.js
@@ -0,0 +1,18 @@
+document.querySelector(".toggle-menu").addEventListener("click",function(){
+  document.querySelector(".main-container").classList.toggle("open");
+})
+
+/*document.querySelector(".toggle-menu").addEventListener("click",function(){
+  let flag = "close";
+  document.querySelector(".toggle-menu").innerHTML = flag;
+  flag = "dehaze";
+  restart();
+});
+
+
+function restart(){
+  document.querySelector(".toggle-menu").addEventListener("click",function(){
+  document.querySelector(".toggle-menu").innerHTML = flag;
+  //start();
+})
+}*/
